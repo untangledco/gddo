3eff68d1541da5612ee69fba1af880ba76cb77b7
Adnan Maolood <adnan@maolood.com>
2023-12-13 11:55:13 -0500

    internal/server: Reduce number of platforms

--- internal/server/platforms.go
+++ internal/server/platforms.go
@@ -2,19 +2,9 @@ package server
 
 var (
 	platforms = []string{
-		"aix/ppc64",
-		"android/amd64",
 		"darwin/amd64",
-		"dragonfly/amd64",
-		"freebsd/amd64",
-		"illumos/amd64",
-		"ios/arm64",
 		"js/wasm",
 		"linux/amd64",
-		"netbsd/amd64",
-		"openbsd/amd64",
-		"plan9/amd64",
-		"solaris/amd64",
 		"windows/amd64",
 	}
 	platformsMap = make(map[string]struct{})
